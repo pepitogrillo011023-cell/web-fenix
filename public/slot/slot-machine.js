e7c2cf3b736f4959bb14e0cce374efaedbd38ae3
pepitogrillo011023-cell <pepitogrillo011023@gmail.com>
2026-06-05 04:06:38 -0300

    Update slot-machine.js

--- public/slot/slot-machine.js
+++ public/slot/slot-machine.js
@@ -4,7 +4,6 @@ function SlotMachine(container, reels, callback, options) {
     const self = this;
     
     function createReelElm(config) {
-        console.log("Creando rodillo para:", config.imageSrc);
         const div = document.createElement('div');
         div.classList.add('reel');
         const ul = document.createElement('ul');
@@ -16,7 +15,7 @@ function SlotMachine(container, reels, callback, options) {
             li.style.backgroundPosition = `0px -${i * 150}px`;
             ul.appendChild(li);
         }
-        config['element'] = ul; // Esto es el <ul> con clase .strip
+        config['element'] = ul;
         div.appendChild(ul);
         return div;
     }
@@ -28,15 +27,15 @@ function SlotMachine(container, reels, callback, options) {
     container.appendChild(divSlots);
 
     self.startSpinAnimation = function() {
-        console.log("Iniciando giro...");
         reels.forEach(r => {
-            console.log("Aplicando clase spin a:", r.element);
-            r.element.classList.add('spin');
+            const elm = r.element;
+            elm.style.marginTop = ""; // LIMPIAMOS EL ESTILO INLINE PARA QUE GIRE
+            elm.classList.remove('stop');
+            elm.classList.add('spin');
         });
     };
 
     self.stopSpinAndShowResult = function(resultadoArray) {
-        console.log("Parando giros...");
         reels.forEach((reel, index) => {
             const ul = reel.element;
             const simbolo = reel.symbols.find(s => s.name === resultadoArray[index]);
@@ -44,10 +43,10 @@ function SlotMachine(container, reels, callback, options) {
 
             setTimeout(() => {
                 ul.classList.remove('spin');
-                ul.style.marginTop = `-${finalPos}px`;
-                console.log("Rodillo " + index + " frenado en:", finalPos);
+                ul.classList.add('stop');
+                ul.style.marginTop = `-${finalPos}px`; // Aquí aplicamos el resultado final
                 if (index === reels.length - 1 && callback) callback();
-            }, 1000 + (index * 500));
+            }, 1000 + (index * 400));
         });
     };
     return self;
