35dda2651a4dfbe0490a8493465a8ae077fa34aa
pepitogrillo011023-cell <pepitogrillo011023@gmail.com>
2026-06-05 22:57:40 -0300

    Update Retiro.js

--- models/Retiro.js
+++ models/Retiro.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const RetiroSchema = new mongoose.Schema({
     fecha: String,
     turno: String,
-    retiros: Array, // <--- Al poner 'Array' genérico, Mongoose no filtrará ni descartará los datos
+    retiros: Array,
     fechaCreacion: { type: Date, default: Date.now }
-});
+}, { strict: false }); // <--- LA MAGIA ESTÁ ACÁ: Desactiva el modo caprichoso de Mongoose
 
 module.exports = mongoose.models.Retiro || mongoose.model('Retiro', RetiroSchema);
