b7af3ee16a9f7606ce74c9df084da6a5c79cdc74
pepitogrillo011023-cell <pepitogrillo011023@gmail.com>
2026-06-05 22:38:02 -0300

    Update Retiro.js

--- models/Retiro.js
+++ models/Retiro.js
@@ -1,15 +1,9 @@
-// models/Retiro.js
 const mongoose = require('mongoose');
 
 const RetiroSchema = new mongoose.Schema({
-    fecha: { type: String, required: true }, // Ejemplo: "2026-06-05"
-    turno: { type: String, required: true }, // "Mañana", "Tarde", "Noche"
-    retiros: [{
-        cliente: String,
-        monto: Number,
-        hora: String,
-        verificado: Boolean
-    }],
+    fecha: String,
+    turno: String,
+    retiros: Array, // <--- Al poner 'Array' genérico, Mongoose no filtrará ni descartará los datos
     fechaCreacion: { type: Date, default: Date.now }
 });
 
