ec9dd57065dc738f5dfe69b21b86040d0a8ce8be
pepitogrillo011023-cell <pepitogrillo011023@gmail.com>
2026-06-10 11:46:27 -0300

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1360,5 +1360,6 @@ socket.on('resultado_carga_cliente', (data) => {
                 alert(`Tu carga de $${data.monto} en ${data.plataforma} fue rechazada.`);
             }
         }
+        }
     
 });
