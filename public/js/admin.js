072fb35859d84e587f1104890fad22d012682113
pepitogrillo011023-cell <pepitogrillo011023@gmail.com>
2026-06-11 12:47:42 -0300

    Update admin.js

--- public/js/admin.js
+++ public/js/admin.js
@@ -1693,4 +1693,4 @@ async function actualizarBadgeSilencioso() {
         console.log("Error silencioso al actualizar el badge:", e);
     }
 }
-console.log("🚨 ¡SI PODÉS LEER ESTO, EL ARCHIVO ADMIN.JS SE ACTUALIZÓ CORRECTAMENTE! 🚨");
+  
